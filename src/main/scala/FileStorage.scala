c7c37614230e56dda1313100f27058d547aef47f
Michael Shestero <shestero@gmail.com>
2023-05-14 16:26:30 +0300

    FileStorage

--- src/main/scala/FileStorage.scala
+++ src/main/scala/FileStorage.scala
@@ -1,26 +1,38 @@
 import io.minio.{GetObjectResponse, StatObjectResponse}
 
-import java.io.{File, FileInputStream, FileOutputStream, InputStream}
+import java.io.{File, FileInputStream, FileOutputStream, InputStream, PrintWriter}
 import java.nio.file.{Files, Path}
+import scala.io.Source
 
 class FileStorage extends CacheAPI {
 
   val dir = "./storage"
+  val defaultContentType = "binary/octet-stream"
 
-  override def put(bucket: String, id: String, inputStream: InputStream, size: Long, contentType: String = "binary/octet-stream"): Unit = {
+  override def put(bucket: String,
+                   id: String, inputStream: InputStream,
+                   size: Long,
+                   contentType: String = defaultContentType
+                  ): Unit = {
     val bucketPath = s"$dir/$bucket"
     // create it if not exists
     val f = new File(bucketPath)
     if (!f.exists()) f.mkdir()
 
     val fileName = s"$bucketPath/$id"
-    val output = Path.of(fileName)
-    Files.copy(inputStream, output)
+    Files.copy(inputStream, Path.of(fileName))
+
+    // save contentType
+    new PrintWriter(s"$fileName.mime") {
+      write(contentType)
+      close()
+    }
   }
 
   override def get(bucket: String, id: String): (String, InputStream) = {
     val fileName = s"$dir/$bucket/$id"
-    "binary/octet-stream" -> new FileInputStream(fileName)
+    val contentType = Source.fromFile(s"$fileName.mime").getLines().nextOption() getOrElse defaultContentType
+    contentType -> new FileInputStream(fileName)
   }
 
   override def delete(bucket: String, id: String): Unit = {
